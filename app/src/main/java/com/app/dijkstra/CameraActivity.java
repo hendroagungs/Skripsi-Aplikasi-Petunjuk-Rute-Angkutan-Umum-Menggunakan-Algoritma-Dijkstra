e38d22ad360db9e64f411a13c86135944133e89a
AlanChou <alan.durant.chou@gmail.com>
2016-08-13 02:32:34 +0800

    Initial commit

---
+++ app/src/main/java/com/app/dijkstra/CameraActivity.java
@@ -0,0 +1,66 @@
+package com.app.dijkstra;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+import android.net.Uri;
+import android.os.Bundle;
+/**
+ * Created by Hsin-ping on 2016/8/12.
+ */
+public class CameraActivity extends MainActivity {
+
+    EditText addrField;
+    Button btnConnect;
+    VideoView streamView;
+    MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+
+        addrField = (EditText)findViewById(R.id.addr);
+        btnConnect = (Button)findViewById(R.id.connect);
+        streamView = (VideoView)findViewById(R.id.streamview);
+
+        btnConnect.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String s = addrField.getEditableText().toString();
+                playStream(s);
+            }});
+
+    }
+
+    private void playStream(String src){
+        Uri UriSrc = Uri.parse(src);
+        if(UriSrc == null){
+            Toast.makeText(CameraActivity.this,
+                    "UriSrc == null", Toast.LENGTH_LONG).show();
+        }else{
+            streamView.setVideoURI(UriSrc);
+            mediaController = new MediaController(this);
+            streamView.setMediaController(mediaController);
+            streamView.start();
+
+            Toast.makeText(CameraActivity.this,
+                    "Connect: " + src,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        streamView.stopPlayback();
+    }
+
+}
